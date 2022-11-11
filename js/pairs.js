5d49857b1fc9ece7b9f5832c4399fbf192b80f1d
Ozeymandias <aaderikha@yandex.ru>
2022-11-11 23:32:18 +0300

    bbv
    vvvvv

---
+++ js/pairs.js
@@ -0,0 +1,183 @@
+let c = 110;
+let timecount = c; 
+let count = 0;
+let check3 = document.querySelector('#check3'); 
+check3.addEventListener('click',gameStart);
+let input = document.querySelector('#input');
+let finishcont = document.createElement('div');
+finishcont.setAttribute('id','finish');
+let time;
+let timeText = document.createElement('h1');
+timeText.textContent = 'Нажмите чтобы начать';
+let timeDiv = document.querySelector('#time');
+timeText.textContent = `Введите чётное число от 4 до 16`;
+timeDiv.append(timeText);
+
+function win()
+{
+    clearInterval(time);
+    timecount = c;
+    let rama = document.querySelector('#rama'); 
+    let gameCont = document.querySelector('#container');
+    rama.removeChild(gameCont);
+    rama.appendChild(finishcont);
+    let finishText = document.createElement('div');
+    finishText.textContent = 'Ура, вы выиграли';
+    finishcont.appendChild(finishText);
+    check3.style.display = 'block';
+    let restartbutton = document.createElement('button');
+    restartbutton.textContent = 'Забрать выигрыш';
+    restartbutton.style.display = 'block';
+    finishcont.appendChild(restartbutton);
+    restartbutton.addEventListener('click',()=>
+    {
+        let erocontainer = document.querySelector('#ero');
+        erocontainer.style.display = 'block';
+        
+    })
+}
+
+function lose()
+{
+    timecount = c;
+    let rama = document.querySelector('#rama'); 
+    let gameCont = document.querySelector('#container');
+    rama.removeChild(gameCont);
+    let finishText = document.createElement('div');
+    finishText.textContent = 'Ты проебал';
+    rama.appendChild(finishText);
+    check3.style.display = 'block';
+    let restartbutton = document.createElement('button');
+    restartbutton.textContent = 'Попробовать снова';
+    rama.appendChild(restartbutton);
+    restartbutton.addEventListener('click',()=>
+    {
+        location.reload();
+    })
+}
+
+function openedCheck()
+{
+    let openCount = 0;
+    let checkedCards = document.querySelectorAll('.card');
+    for(let i=0; i<checkedCards.length;i++)
+    {
+        if(checkedCards[i].classList.contains('opened'))
+        {
+            ++openCount;
+            console.log(openCount);
+        }
+        if(openCount == checkedCards.length)
+        {
+            clearInterval(time);
+            timeText.textContent = '';
+            win();
+        }
+    }
+}
+
+function gameStart()
+{ 
+    let firstcard;
+    let secondcard;
+    let firstclass;
+    let secondclass;
+    count = 0;
+    let cardsArray = [
+    ['.card1','url(img/1.jpg)'],['.card1','url(img/1.jpg)'],
+    ['.card2','url(img/2.jpg)'],['.card2','url(img/2.jpg)'],
+    ['.card3','url(img/3.jpg)'],['.card3','url(img/3.jpg)'],
+    ['.card4','url(img/4.jpg)'],['.card4','url(img/4.jpg)'],
+    ['.card5','url(img/5.jpg)'],['.card5','url(img/5.jpg)'],
+    ['.card6','url(img/6.jpg)'],['.card6','url(img/6.jpg)'],
+    ['.card7','url(img/7.jpg)'],['.card7','url(img/7.jpg)'],
+    ['.card8','url(img/8.jpg)'],['.card8','url(img/8.jpg)']];
+    clearInterval(time);
+    check3.style.display = 'none';
+    let rama = document.querySelector('#rama');
+    rama.innerHTML = "";
+    let contain = document.createElement('div');
+    contain.setAttribute('id', 'container');
+    rama.appendChild(contain);
+    let randarray = [];
+    let len = input.value;
+    let lens = parseInt(len);
+    if(lens%2 != 0 || lens > 16 || lens < 4)
+    {
+        timeText.textContent = 'Введите чётное число от 4 до 16';
+        location.reload();
+    }
+    else
+    {
+        let cardpop = cardsArray.length-lens;
+        for(let k = 0; k < cardpop; k++)
+        {
+            cardsArray.pop();
+        }
+        for(let i = 0; i < lens; i++)
+        {
+            let randomindex = Math.round(Math.random()*(cardsArray.length-1));
+            randarray.push(cardsArray[randomindex]);
+            cardsArray.splice(randomindex,1);
+        }
+        for(let k = 0; k < randarray.length; k++)
+        {
+            let newcard = document.createElement('div');
+            newcard.classList.add('card');
+            newcard.classList.add(randarray[k][0]);
+            contain.appendChild(newcard);
+            newcard.addEventListener('click',()=>{
+                newcard.style.backgroundImage = randarray[k][1];
+                newcard.classList.add('opened');
+                openedCheck();
+                ++count;
+                if(firstclass != secondclass && count == 3)
+                {
+                    firstcard.classList.remove(firstclass);
+                    secondcard.classList.remove(secondclass);
+                    firstcard.classList.remove('opened');
+                    secondcard.classList.remove('opened');
+                    count=1;
+                    firstcard.style.backgroundImage = 'url(img/logo.jpg)';
+                    secondcard.style.backgroundImage = 'url(img/logo.jpg)';
+                }
+                if(firstclass == secondclass && count == 3)
+                {
+                    count=1;
+                }
+                if(count == 1)
+                {
+                    console.log('first');
+                    firstcard = newcard;
+                    firstclass = randarray[k][0];
+                }
+                if(count == 2)
+                {
+                    console.log('second');
+                    secondcard = newcard;
+                    secondclass = randarray[k][0];
+                }
+            })
+        }
+        gameTimer();
+    }
+    
+}
+
+function countdown()
+{
+    --timecount;
+    timeText.textContent = `У вас ещё ${timecount} секунд`;
+    if(timecount== 0)
+        {
+            clearInterval(time);
+            timeText.textContent = '';
+            lose();
+        }
+}
+
+function gameTimer()
+{  
+    clearInterval(time);
+    time = setInterval(countdown,1000);  
+}
---
+++ js/register.js
@@ -0,0 +1,260 @@
+let button = document.querySelector('#button');
+button.addEventListener('click',addAcc);
+
+let warning = document.createElement('p');
+warning.textContent = 'Введите корректные данные';
+warning.setAttribute('id','warning');
+button.after(warning);
+warning.style.display = 'none';
+
+let form = document.querySelector('form');
+let table = document.querySelector('#table');
+let AccName = document.querySelector('#names');
+let AccFamily = document.querySelector('#family');
+let AccFather = document.querySelector('#father');
+let AccDate = document.querySelector('#data');
+let DateArray = AccDate.value.split('-'); 
+let AccYear = document.querySelector('#year'); 
+let AccFaculty = document.querySelector('#faculty'); 
+
+let tableContainer = document.createElement('div')
+tableContainer.classList.add('table-container');
+table.appendChild(tableContainer);
+
+let tableHead = document.createElement('div');
+tableHead.classList.add('table-item');
+tableHead.classList.add('table-head');
+function headDeploy(headObject,text,container)
+{
+    headObject = document.createElement('div');
+    headObject.textContent = text;
+    container.append(headObject);
+}
+let headName, headFamily, headFather,headDate,headYear,headFaculty;
+headDeploy(headName,'Имя бойца',tableHead);
+headDeploy(headFamily,'Фамилия',tableHead);
+headDeploy(headFather,'Отчество',tableHead);
+headDeploy(headDate,'Дата рождения',tableHead);
+headDeploy(headYear,'Год вступления',tableHead);
+headDeploy(headFaculty,'Специальность',tableHead);
+
+tableContainer.appendChild(tableHead);
+let AccContainer = document.createElement('div');
+AccContainer.setAttribute('id','acc-container');
+tableContainer.after(AccContainer);
+let headCont = document.createElement('div');
+let headBut = document.createElement('button');
+headBut.textContent = 'Clear';
+tableHead.append(headCont);
+headCont.append(headBut);
+headBut.addEventListener('click',()=>{
+    AccContainer.innerHTML = '';
+})
+
+let array = [];
+
+function addAcc(e)
+{
+    e.preventDefault();
+    let Name = AccName.value.trim();
+    let Family = AccFamily.value.trim();
+    let Father = AccFather.value.trim();
+    let Faculty = AccFaculty.value.trim();
+    if(Name == "" || Family == "" || Father == "" || Faculty =="" || AccYear.value == "" || AccDate.value == "" || Name.match(/^\d+$/) || Faculty.match(/^\d+$/) || Family.match(/^\d+$/) || Father.match(/^\d+$/))
+    {
+        warning.style.display = 'block';
+    }
+    else
+    {
+            warning.style.display = 'none';
+            let Object = {names:Name,family:Family,father:Father,date:AccDate.value, year:AccYear.value, faculty:Faculty, 
+            deploy ()
+            {
+                let container = document.createElement('div');
+                container.classList.add('table-item');
+                AccContainer.append(container);
+                let accName = document.createElement('div');
+                accName.textContent = this.names;
+                container.append(accName);
+                let accFamily = document.createElement('div');
+                accFamily.textContent = this.family;
+                container.append(accFamily);
+                let accFather = document.createElement('div');
+                accFather.textContent = this.father;
+                container.append(accFather);
+                let accDate = document.createElement('div');
+                let DateArray = this.date.split('-');
+                let DateRight = `${DateArray[2]}.${DateArray[1]}.${DateArray[0]} г.`;
+                accDate.textContent = DateRight;
+                container.append(accDate);
+                let accYear = document.createElement('div');
+                accYear.textContent = this.year;
+                container.append(accYear);
+                let accFaculty = document.createElement('div');
+                accFaculty.textContent = this.faculty;
+                container.append(accFaculty);
+                let deleteDiv = document.createElement('div'); 
+                let deleteButton = document.createElement('button');
+                deleteButton.textContent = 'clear';
+                container.append(deleteDiv);
+                deleteDiv.append(deleteButton);
+                deleteButton.addEventListener('click',()=>{
+                    let par = deleteButton.parentNode;
+                    let parpar = par.parentNode;
+                    parpar.parentNode.removeChild(parpar);
+                })
+            }
+        };
+        Object.deploy();
+        array.push(Object);
+        form.reset();
+    }
+    
+}
+
+let namecount = 0, familycount = 0, fathercount = 0, datecount = 0, yearcount = 0, facultycount =0; 
+function sortName(count)
+{  
+    if(count%2 == 0)
+    {
+        array.sort((prev,next)=>{
+            if(prev.names < next.names) return -1;
+            if (prev.names < next.names) return 1;
+            })   
+    }
+    else if(count%2 ==1)
+    {
+        array.sort((prev,next)=>{
+        if(prev.names > next.names) return -1;
+        if (prev.names > next.names) return 1;
+        })
+    }
+    AccContainer.innerHTML = '';
+    for(let i = 0; i < array.length; i++)
+    {
+        array[i].deploy();
+    }
+}
+function sortFamily(count)
+{  
+    if(count%2 == 0)
+    {
+        array.sort((prev,next)=>{
+            if(prev.family < next.family) return -1;
+            if (prev.family < next.family) return 1;
+            })
+    }
+    else if(count%2 ==1)
+    {
+        array.sort((prev,next)=>{
+        if(prev.family > next.family) return -1;
+        if (prev.family > next.family) return 1;
+        })
+    }
+    AccContainer.innerHTML = '';
+    for(let i = 0; i < array.length; i++)
+    {
+        array[i].deploy();
+    }
+}
+function sortFather(count)
+{  
+    if(count%2 == 0)
+    {
+        array.sort((prev,next)=>{
+            if(prev.father < next.father) return -1;
+            if (prev.father < next.father) return 1;
+            })
+    }
+    else if(count%2 ==1)
+    {
+        array.sort((prev,next)=>{
+        if(prev.father > next.father) return -1;
+        if (prev.father > next.father) return 1;
+        })
+    }
+    AccContainer.innerHTML = '';
+    for(let i = 0; i < array.length; i++)
+    {
+        array[i].deploy();
+    }
+}
+function sortDate(count)
+{  
+    if(count%2 == 0)
+    {
+        array.sort((prev,next)=>{
+            if(prev.date < next.date) return -1;
+            if (prev.date < next.date) return 1;
+            })
+    }
+    else if(count%2 ==1)
+    {
+        array.sort((prev,next)=>{
+        if(prev.date > next.date) return -1;
+        if (prev.date > next.date) return 1;
+        })
+    }
+    AccContainer.innerHTML = '';
+    for(let i = 0; i < array.length; i++)
+    {
+        array[i].deploy();
+    }
+}
+function sortYear(count)
+{  
+    if(count%2 == 0)
+    {
+        array.sort((prev,next)=>{
+            if(prev.year < next.year) return -1;
+            if (prev.year < next.year) return 1;
+            })
+    }
+    else if(count%2 ==1)
+    {
+        array.sort((prev,next)=>{
+        if(prev.year > next.year) return -1;
+        if (prev.year > next.year) return 1;
+        })
+    }
+    AccContainer.innerHTML = '';
+    for(let i = 0; i < array.length; i++)
+    {
+        array[i].deploy();
+    }
+}
+function sortFaculty(count)
+{  
+    if(count%2 == 0)
+    {
+        array.sort((prev,next)=>{
+            if(prev.faculty < next.faculty) return -1;
+            if (prev.faculty < next.faculty) return 1;
+            })
+    }
+    else if(count%2 ==1)
+    {
+        array.sort((prev,next)=>{
+        if(prev.faculty > next.faculty) return -1;
+        if (prev.faculty > next.faculty) return 1;
+        })
+    }
+    AccContainer.innerHTML = '';
+    for(let i = 0; i < array.length; i++)
+    {
+        array[i].deploy();
+    }
+}
+
+let buttonName = document.querySelector('#nameButton');
+buttonName.addEventListener('click',()=>{sortName(namecount);namecount++});
+let buttonFamily = document.querySelector('#familyButton');
+buttonFamily.addEventListener('click',()=>{sortFamily(familycount);familycount++});
+let buttonFather = document.querySelector('#fatherButton');
+buttonFather.addEventListener('click',()=>{sortFather(fathercount);fathercount++});
+let buttonDate = document.querySelector('#dateButton');
+buttonDate.addEventListener('click',()=>{sortDate(datecount);datecount++});
+let buttonYear = document.querySelector('#yearButton');
+buttonYear.addEventListener('click',()=>{sortYear(yearcount);yearcount++});
+let buttonFaculty = document.querySelector('#facultyButton');
+buttonFaculty.addEventListener('click',()=>{sortYear(facultycount);facultycount++});
